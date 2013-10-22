612b8795190763889dedf5e66e11f6e894ae63d5
Marco Davi <mardavi84@gmail.com>
2013-10-22 14:33:00 +0200

    Fuck yeah: used Spock Mocks :)

--- src/test/groovy/org/mdavi/neuralnetwork/hopfield/network/HopfieldNetworkTest.groovy
+++ src/test/groovy/org/mdavi/neuralnetwork/hopfield/network/HopfieldNetworkTest.groovy
@@ -1,7 +1,11 @@
 package org.mdavi.neuralnetwork.hopfield.network
 
+import org.mdavi.neuralnetwork.hopfield.coach.Coach;
+import org.mdavi.neuralnetwork.hopfield.coach.CoachCreator;
 import org.mdavi.neuralnetwork.hopfield.coach.HopfieldCoachCreator;
 import org.mdavi.neuralnetwork.hopfield.network.components.layer.HopfieldLayerCreator
+import org.mdavi.neuralnetwork.hopfield.network.components.layer.Layer;
+import org.mdavi.neuralnetwork.hopfield.network.components.layer.LayerCreator;
 import org.mdavi.neuralnetwork.hopfield.network.components.neuron.HopfieldNeuronCreator
 
 import spock.lang.Specification
@@ -9,17 +13,26 @@ import spock.lang.Specification
 class HopfieldNetworkTest extends Specification
 {
   HopfieldNetwork network
+  def coachCreator = Mock(CoachCreator)
+  def layerCreator = Mock(LayerCreator)
+  def coach = Mock(Coach)
+  def layer = Mock(Layer)
+  
   def setup () {
-    network = new HopfieldNetwork(new HopfieldCoachCreator(), new HopfieldLayerCreator(new HopfieldNeuronCreator()))
+    network = new HopfieldNetwork(coachCreator, layerCreator)
   }
   
   def "Hopfield nework training" () {
     when: "The hopfield network is trained to recognize a pattern"
       network.train(pattern)
     then: "it changes the connection weights accordingly"
+      interaction {
+        coachExpectations (pattern, initial, expectedWeights)
+      }
       network.weights == expectedWeights
     where:
       pattern << [[0,1,0,1]]
+      initial << [[[0, 0, 0, 0], [0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]]]
       expectedWeights << [
         [[0, -1, 1, -1], [-1, 0, -1, 1],[1, -1, 0, -1],[-1, 1, -1, 0]]
         ]
@@ -28,18 +41,36 @@ class HopfieldNetworkTest extends Specification
   def "Hopfield nework recognition" () {
     when: "The hopfield network is trained to recognize a pattern"
       network.train(pattern)
+      def output = network.recognize(pattern)
     then: "can recognize that pattern and the inverse one"
-      network.recognize(pattern) == pattern
+      interaction {
+        coachExpectations (pattern, initial, expectedWeights)
+        layerExpectations(pattern, expectedWeights)
+      }
+      output == pattern
     where:
-      pattern << [[0,1,0,1], [1,0,1,0]]
+      pattern << [[0,1,0,1]]
+      initial << [
+        [[0, 0, 0, 0], [0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]]
+        ]
+      expectedWeights << [
+        [[0, -1, 1, -1], [-1, 0, -1, 1],[1, -1, 0, -1],[-1, 1, -1, 0]]
+        ]
   }
+  
   
   def "Hopfield nework multipattern recognition" () {
     when: "The hopfield network is trained to recognize more patterns"
       network.train(pattern[0])
       network.train(pattern[1])
+      def output = network.recognize(input)
     then: "can recognize both trained patterns and the inverse ones"
-      network.recognize(input) == input
+      interaction {
+        coachExpectations (pattern[0], initial, expectedWeights[0])
+        coachExpectations (pattern[1], expectedWeights[0], expectedWeights[1])
+        layerExpectations(input, expectedWeights[1])
+      }
+      output == input
     where:
       pattern << [
         [[0,1,0,1], [1,0,0,1]],
@@ -47,6 +78,25 @@ class HopfieldNetworkTest extends Specification
       input << [
         [0,1,1,0],
         [1,0,1,0]]
+      initial << [
+        [[0, 0, 0, 0], [0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]],
+        [[0, 0, 0, 0], [0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]]
+        ]
+      expectedWeights << [
+        [[[0, -1, 1, -1], [-1, 0, -1, 1],[1, -1, 0, -1],[-1, 1, -1, 0]], [[0, -2, 0, 0], [-2, 0, 0, 0],[0, 0, 0, -2],[0, 0, -2, 0]]], 
+        [[[0, -1, 1, -1], [-1, 0, -1, 1],[1, -1, 0, -1],[-1, 1, -1, 0]], [[0, -2, 0, 0], [-2, 0, 0, 0],[0, 0, 0, -2],[0, 0, -2, 0]]]
+        ]
       
+  }
+
+  private def layerExpectations (pattern, expectedWeights) {
+    1 * layerCreator.newLayer() >>> [layer]
+    1 * layer.activateFor(pattern, expectedWeights) >>> [pattern]
+  }
+  
+  private def coachExpectations (pattern, initial, expectedWeights) {
+    1 * coachCreator.newCoach() >>> [coach]
+    1 * coach.computeTrainingSequence(pattern) >>> [initial]
+    1 * coach.obtainWeights(initial) >>> [expectedWeights]
   }
 }
--- src/test/groovy/org/mdavi/neuralnetwork/hopfield/network/components/layer/LayerTest.groovy
+++ src/test/groovy/org/mdavi/neuralnetwork/hopfield/network/components/layer/LayerTest.groovy
@@ -1,16 +1,24 @@
 package org.mdavi.neuralnetwork.hopfield.network.components.layer
 
 import org.mdavi.neuralnetwork.hopfield.network.NetworkSize;
+import org.mdavi.neuralnetwork.hopfield.network.components.neuron.HopfieldNeuron
 import org.mdavi.neuralnetwork.hopfield.network.components.neuron.HopfieldNeuronCreator
+import org.mdavi.neuralnetwork.hopfield.network.components.neuron.Neuron;
+import org.mdavi.neuralnetwork.hopfield.network.components.neuron.NeuronCreator;
 
 import spock.lang.Specification;
 
 class LayerTest extends Specification
 {
   def layerCreator
+  def neuronCreator = Mock(NeuronCreator)
+  def neuron1 = Mock(Neuron)
+  def neuron2 = Mock(Neuron)
+  def neuron3 = Mock(Neuron)
+  def neuron4 = Mock(Neuron)
   
   def setup () {
-    layerCreator = new HopfieldLayerCreator(new HopfieldNeuronCreator())
+    layerCreator = new HopfieldLayerCreator(neuronCreator)
   }
 
   def "HopfieldLayer description" () {
@@ -23,9 +31,23 @@ class LayerTest extends Specification
   def "HopfieldLayer output" () {
     when: "The layer is activated with an input pattern"
       HopfieldLayer layer = layerCreator.newLayer()
+      def output = layer.activateFor(pattern, weights)
     then: "it computes an out pattern"
-      layer.activateFor(pattern, weights) == pattern
+      interaction {
+        NetworkSize.SIZE * neuronCreator.newNeuron() >>> [neuron1, neuron2, neuron3, neuron4]
+        1 * neuron1.activateFor(pattern, weights[0]) >>> [neuronOutput[0]]
+        1 * neuron2.activateFor(pattern, weights[1]) >>> [neuronOutput[1]]
+        1 * neuron3.activateFor(pattern, weights[2]) >>> [neuronOutput[2]]
+        1 * neuron4.activateFor(pattern, weights[3]) >>> [neuronOutput[3]]
+      }
+      output == pattern
+      
     where:
+      neuronOutput << [
+        [-2, 1, -2, 1],
+        [-2, 0, -2, 0],
+        [0, -2, -2, 0],
+        ]
       weights << [
         [[0, -1, 1, -1], [-1, 0, -1, 1],[1, -1, 0, -1],[-1, 1, -1, 0]],
         [[0, -2, 0, 0], [-2, 0, 0, 0],[0, 0, 0, -2],[0, 0, -2, 0]],
